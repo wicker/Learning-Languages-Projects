799f6f70f02a5823421202135515cd8ac921b1af
Jenner Hanni <jeh.wicker@gmail.com>
2013-09-20 13:04:42 -0700

    Moved over the C program from Learning-C-and-Cpp repo.

---
+++ c/insertion/insertion.c
@@ -0,0 +1,21 @@
+/* Author: Jenner Hanni
+ * Language: C
+ * Purpose: To write insertion and merge sorts.
+ * License: MIT
+ */
+
+#include <stdio.h>
+
+int main() {
+
+  int an_array[] = {1,2,9,4,8,5,6,3,8,1};
+
+  int i;
+  int array_length = sizeof(an_array)/sizeof(int);
+  int int_length = sizeof(int);
+
+  printf("sizeof(int): %d\n",int_length);
+  printf("array_length: %d\n",array_length);
+
+  return 0;
+}
