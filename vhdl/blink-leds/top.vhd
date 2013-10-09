9a7f89d161f0ecdd033634c15e9d72bf8bfc9c25
wicker <wicker@summerisle.(none)>
2013-10-09 16:00:34 -0700

    Structure with comments.

--- vhdl/blink-leds/top.vhd
+++ vhdl/blink-leds/top.vhd
@@ -11,16 +11,25 @@ use ieee.numeric_std.all
 entity blink_led is
   port (
     clk : in  std_logic;
-    led : out std_logic_vector(7 downto 0);
     btn : in  std_logic_vector(1 downto 0);
+    led : out std_logic_vector(7 downto 0);
   );
 end blink_led;
 
 -----------------------------------------
 
 architecture behavioral of blink_led is 
+
+  -- signals, constants, etc. other declarations
+
 begin
 
+  -- concurrent signal assignments
+  -- process statements
+  -- conditional signal assignments
+  -- selected signal assignment
+  -- component instantiation statement
+  -- generate statement
 
 end behavioral;
 
