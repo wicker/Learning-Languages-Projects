197726ceb4416ce2e3f75a92dd88c2ebf68ce929
wicker <wicker@summerisle.(none)>
2013-10-09 16:15:25 -0700

    Simple blink LED example.

--- vhdl/blink-leds/blink_led.vhd
+++ vhdl/blink-leds/blink_led.vhd
@@ -10,26 +10,31 @@ use ieee.numeric_std.all
 
 entity blink_led is
   port (
-    clk : in  std_logic;
-    btn : in  std_logic_vector(1 downto 0);
-    led : out std_logic_vector(7 downto 0);
+    clock_50  : in  std_logic;
+    key       : in  std_logic_vector(1 downto 0);
+    led       : out std_logic_vector(7 downto 0);
   );
 end blink_led;
 
 -----------------------------------------
 
 architecture behavioral of blink_led is 
-
-  -- signals, constants, etc. other declarations
-
 begin
 
-  -- concurrent signal assignments
-  -- process statements
-  -- conditional signal assignments
-  -- selected signal assignment
-  -- component instantiation statement
-  -- generate statement
+  process (clock_50) begin
+
+    if rising_edge(clock_50) then
+
+      case key is 
+        when "11" ==> led <= "00011000";
+        when "01" ==> led <= "00001111";
+        when "10" ==> led <= "11110000";
+        when "00" ==> led <= "11111111";
+      end case;
+
+    end if;
+
+  end process;
 
 end behavioral;
 
